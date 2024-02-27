fd73a0fd1a0e78c3150780d527cec951b4b6b882
ShemArmand <131203757+ShemArmand@users.noreply.github.com>
2024-02-27 19:59:07 +0200

    Add files via upload

---
+++ Navigation.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <Contact/>
+        </NavigationContainer>
+    )
+}
